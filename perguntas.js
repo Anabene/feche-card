5d078f1be8b096549b60ff4995e1cfb5228f3585
Anabene <165837536+Anabene@users.noreply.github.com>
2025-08-13 08:15:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cantores',
+    'Qual o nome completo de Taylor Swift?',
+    'Taylor Alison Swift.'
+)
+
+criaCartao(
+    'Cantores',
+    'Qual o nome completo de Beyoncé?',
+    'Beyoncé Giselle Knowles-Carter'
+)
+
+criaCartao(
+    'Cantores',
+    'Qual é um dos álbuns mais aclamados de Beyoncé, lançado em 2016?',
+    'Lemonade.'
+)
+
+criaCartao(
+    'Cantores',
+    'Qual é o nome da música com o rapper J. Balvin que foi um grande hit em 2017?',
+    'Mi Gente'
+)
